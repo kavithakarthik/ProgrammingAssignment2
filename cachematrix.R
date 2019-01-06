f2b2757387936484f335c45cd49abf16ef17736c
Kavitha Jayakumar <kjayakumar@salesforce.com>
2019-01-06 09:37:59 -0700

    Assignment 2 - cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Using Lexical scoping to cache values of a matrix and its calculated inverse. 
+## Use the cache if no change happens to the matrix.
+## If original matrix changes, recalculate the inverse.
 
-## Write a short comment describing this function
 
+## Defines and returns a list of functions to set, get, setinverse and getinverse 
+## of a given matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse<- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'.
+## Returns the inverse from cache if present, else computes the inverse,
+## caches it and returns the inverse.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached value")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
